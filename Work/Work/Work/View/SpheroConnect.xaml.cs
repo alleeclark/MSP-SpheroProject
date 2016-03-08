14bb9e19d7a3e199aae0cfb05bce058f97ea2d6e
alleeclark <allee.clark@bison.howard.edu>
2016-03-08 00:33:28 -0500

    Working On Conecting

---
+++ Work/Work/Work/View/SpheroConnect.xaml.cs
@@ -0,0 +1,113 @@
+﻿using RobotKit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Work.View
+{
+    public partial class SpheroConnect : ContentPage
+    {
+        public const string ConnectedToSpehro = "Conntect To Sphero";
+        public const string ConnectingToSphero = "Connecting To Sphero";
+        public const string NotConnectedToSphero = "Not Connected";
+
+        Sphero my_robot = null;
+        Switch something = new Switch();
+
+        public SpheroConnect()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedTo(e);
+
+        //    SetUpConnection();
+        //    Application app = Application.Current;
+        //}
+
+        //protected override void OnNavigatedFrom(NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedFrom(e);
+
+        //    ShutDownRobotConnection();
+
+        //    Application app = Application.Current;
+        //}
+
+        public void SetUpConnection()
+        {
+            SpheroName.Text = NotConnectedToSphero;
+
+            RobotProvider provider = RobotProvider.GetSharedProvider();
+            provider.DiscoveredRobotEvent += DicsoveredRobot;
+            provider.NoRobotsEvent += NoRobotsDiscovered;
+            provider.ConnectedRobotEvent += RobotConnected;
+            provider.FindRobots();
+        }
+        private void ShutDownRobotConnection()
+        {
+            if (my_robot != null)
+            {
+                my_robot.SensorControl.StopAll();
+                my_robot.Sleep();
+                //ConnectionToggle.OffContent = "Disconnected";
+                SpheroName.Text = NotConnectedToSphero;
+            }
+        }
+        private void DicsoveredRobot(object sender, Robot robot)
+        {
+            Debug.WriteLine(string.Format("Discovered \"{0}\"", robot.BluetoothName));
+
+            if (my_robot == null)
+            {
+                RobotProvider provider = RobotProvider.GetSharedProvider();
+                provider.ConnectRobot(robot);
+
+                //ConnectionToggle.IsToggled = IsEnabled;
+                //ConnectionToggle.OnContent = "Connected";
+                my_robot.SetRGBLED(255, 2525, 255);
+                SpheroName.Text = string.Format(ConnectedToSpehro, robot.BluetoothName);
+            }
+        }
+
+        private void NoRobotsDiscovered(object sender, EventArgs e)
+        {
+            Debug.WriteLine("No Sphero Paired");
+            DisplayAlert("Alert", "No Sphero Paired", "OK");
+        }
+        private void RobotConnected(object sender, Robot robot)
+        {
+            Debug.WriteLine(string.Format("Connected to {0}", robot));
+            //ConnectionToggle.OnContent = "Connected";
+
+            my_robot.SetRGBLED(255, 255, 255);
+            SpheroName.Text = string.Format(ConnectedToSpehro, robot.BluetoothName);
+
+        }
+        private void ConnectionToggle_Toggled(object sender, ToggledEventArgs args)
+        {
+            Debug.WriteLine("Connection Toggled: ");
+            //ConnectionToggle.OnContent = "Connecting...";
+            if (args.Value)
+            {
+                if (my_robot == null)
+                {
+                    SetUpConnection();
+                }
+                else
+                {
+                    ShutDownRobotConnection();
+                }
+            }
+
+        }
+    }
+}
+
